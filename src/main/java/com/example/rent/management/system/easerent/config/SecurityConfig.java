cce31b5deea2f6df7594df55acf8d20c8f359dfd
JNE5192 <juhilee.nazare9915@gmail.com>
2024-08-18 00:02:01 +0530

    Added TODO related to Spring Security

--- src/main/java/com/example/rent/management/system/easerent/config/SecurityConfig.java
+++ src/main/java/com/example/rent/management/system/easerent/config/SecurityConfig.java
@@ -6,7 +6,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+
 
 @Configuration
 @EnableWebSecurity
@@ -17,7 +17,7 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 	
-	
+	//TODO: Learn more about CSRF and Spring Security
 	@Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
