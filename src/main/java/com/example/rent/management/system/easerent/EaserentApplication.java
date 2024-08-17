0d9f2234b1caca228d2e5f6c1eb745fe137ecc33
JNE5192 <juhilee.nazare9915@gmail.com>
2024-08-17 19:51:51 +0530

    create repository

---
+++ src/main/java/com/example/rent/management/system/easerent/EaserentApplication.java
@@ -0,0 +1,13 @@
+package com.example.rent.management.system.easerent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EaserentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EaserentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/rent/management/system/easerent/EaserentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rent.management.system.easerent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EaserentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
